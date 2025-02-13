0f9eb1f1e860c4c60f652afddad69ab15b8cdbff
Suraj Patel <surajpatelsuraj55@gmail.com>
2025-02-14 00:17:37 +0530

    feat: vercel analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,14 +1,12 @@
 import "./globals.css";
+import { Analytics } from "@vercel/analytics/react";
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en" className={`scroll-smooth`}>
       <body className={`antialiased bg-neutral-50 text-neutral-900`}>
         {children}
+        <Analytics />
       </body>
     </html>
   );
